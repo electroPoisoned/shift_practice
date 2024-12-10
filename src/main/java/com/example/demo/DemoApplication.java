29f362cf9e8fa32dfb01c9bb6adfed71f3a079e3
electroPoisoned <154272666+electroPoisoned@users.noreply.github.com>
2024-12-10 12:32:40 +0300

    swagger

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+@OpenAPIDefinition
 @SpringBootApplication
 public class DemoApplication {
 
